cd44440b554d4dbdd447e881404115690610a0a4
LuaN1409 <marestrepoo@unal.edu.co>
2025-01-13 20:06:26 -0500

    ...

--- Practica_1/Clases/investigador.py
+++ Practica_1/Clases/investigador.py
@@ -3,15 +3,15 @@ from Clases.equipo import Equipo
 
 
 class Investigador(Empleado):
-    print("Ingrese los datos del equipo que desee agregar")
-    nombre = input("Nombre del equipo: ")
-    numero_placa = input("Numero de placa del equipo: ")
-    fecha_compra = input("Fecha de compra del quipo: ")
-    valor_compra = input("valor de compra de quipo (DD/MM/AAAA): ")
     
-    equipo = Equipo(nombre, numero_placa, fecha_compra, valor_compra)
-    
-    def solicitar_agregar_equipo(self, equipo):
+    def solicitar_agregar_equipo(self):
+        print("Ingrese los datos del equipo que desee agregar")
+        nombre = input("Nombre del equipo: ")
+        numero_placa = input("Numero de placa del equipo: ")
+        fecha_compra = input("Fecha de compra del quipo: ")
+        valor_compra = input("valor de compra de quipo (DD/MM/AAAA): ")
+        equipo = Equipo(nombre, numero_placa, fecha_compra, valor_compra)
+        
         if not isinstance(equipo, Equipo):
             print("El quipo solicitado no se encuentra")
             return False
