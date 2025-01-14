2f8e1d3fa99668b3d37eb772c7b693f1b3ffa7c0
Camila Rios <mriosm@unal.edu.co>
2025-01-14 16:50:08 -0500

    Recuperado

--- Practica_1/Clases/administrador.py
+++ Practica_1/Clases/administrador.py
@@ -1,6 +1,7 @@
+from Clases.inventario import Inventario
 from Clases.empleado import Empleado
 from Clases.fecha import Fecha
-from Listas.doble_list import DoubleList
+from Listas.doble_node import DoubleNode
 from Clases.solicitud import Solicitud
 from Listas.lista_simple import List
 
@@ -9,9 +10,7 @@ class Administrador(Empleado):
     def __str__(self):
         super().__str__()
     
-    def crear_usuario(self, nombre, identificacion, fecha_nacimiento, ciudad_nacimiento, telefono, email, direccion, password, descripcion):
-        super().__init__(nombre, identificacion, fecha_nacimiento, ciudad_nacimiento, telefono, email, direccion, password, descripcion)
-        
+    def crear_usuario():
         nombre = input("Nombre: ")
         identificacion = int(input("Número de Identificación: "))
         fecha_ = input("Fecha de Nacimiento (dd/mm/aaaa): ")
@@ -19,7 +18,7 @@ class Administrador(Empleado):
         fecha_nacimiento = Fecha(dia, mes, anio)
         ciudad_nacimiento = input("Ciudad de Nacimiento: ")
         telefono = int(input("Teléfono: "))
-        correo = input("Correo Electrónico: ")
+        email = input("Correo Electrónico: ")
         print("Ingrese dirección de vivienda")
         calle = input("Calle: ")
         nomenclatura = input("Nomenclatura: ")
@@ -31,13 +30,8 @@ class Administrador(Empleado):
         password = input("Ingrese una contraseña: ")
         descripcion = input("Ingrese el rol del empleado: ")
         
-        empleado = Empleado(nombre, identificacion, fecha_nacimiento, ciudad_nacimiento, telefono, correo, direccion, password, descripcion)
+        empleado = Empleado(nombre, identificacion, fecha_nacimiento, ciudad_nacimiento, telefono, email, direccion, password, descripcion)
         super().empleados.add_last(empleado)
-        
-#Esto está gregando y asociando la id con la contraseña y rol, para lo del inicio de sesión y el reconocimiento del rol
-#De igual modo esto es para lo de Password.txt
-        self._password[identificacion] = (password, descripcion)
-        print(f"El usuario del empleado {nombre} fue correctamente creado y agregado a la base de datos")
     
     def eliminar_usuario(self, identificacion_):
         identificacion_ = input("Ingrese la identificación del usuario a eliminar: ")
@@ -84,8 +78,6 @@ class Administrador(Empleado):
                 return
             previous = current
             current = current.get_Next()
-
-    
         
     def responder_solicitudes(self):
         current = Solicitud.solicitudes.first()
@@ -99,7 +91,8 @@ class Administrador(Empleado):
         while current is not None:
             solicitud = current.get_Data() 
             print("\n--- Solicitud ---")
-            print(f"ID: {solicitud.get_id_solicitud()}")
+            print(f"Empleado: {solicitud.get_empleado().get_nombre()}")
+            print(f"ID: {solicitud.get_empleado().get_id()}")
             print(f"Tipo: {solicitud.get_tipo()}")
             print(f"Estado: {solicitud.get_estado()}")
             print(f"Placa del Equipo: {solicitud.get_numero_placa()}")
@@ -108,11 +101,43 @@ class Administrador(Empleado):
 
             decision = input("¿Aprobar o Rechazar? ").strip().upper()
 
+            eq = Inventario.buscar(solicitud.get_numero_placa())
+            emp = solicitud.get_empleado()
             if decision == "Aprobar":
-                print(solicitud.aprobar_solicitud())
+                if solicitud.get_tipo() == "Agregar":
+                    if eq != -1:
+                        if eq.get_empleado().get_nombre() != None:
+                            print("No se puede Aprobar esta solicitud porque el equipo esta asociado a otro empleado.")
+                            continue
+                        else:
+                            eq.set_empleado(emp)
+                            solicitud.aprobar_solicitud()
+                            print("Solicitud Aprobada con exito.")
+                    else:
+                        print("El numero de placa no existe.")
+                        continue
+                elif solicitud.get_tipo() == "Eliminar":
+                    if eq != -1:
+                        if eq.get_empleado().get_nombre() != solicitud.get_empleado().get_nombre():
+                            print("No se puede Aprobar esta solicitud porque el equipo esta asociado a otro empleado.")
+                            continue
+                        else:
+                            node = DoubleNode(eq)
+                            emp.get_inventario().remove(node) #Eliminar el equipo del inventario
+                            eq.set_empleado(None) #Desasociar el equipo del empleado
+                            solicitud.aprobar_solicitud()
+                            print("Solicitud Aprobada con exito.")
+                    else:
+                        print("El numero de placa no existe.")
+                        continue
+                
+                else:
+                    print("El tipo de la solicitud es invalido.")
+                    continue
 
             elif decision == "Rechazar":
-                print(solicitud.rechazar_solicitud())
+                solicitud.rechazar_solicitud()
+                print("Solicitud Rechazada con exito.")
             else:
                 print("Decisión inválida. Saltando a la siguiente solicitud.")
                 current = current.get_Next()
@@ -128,10 +153,23 @@ class Administrador(Empleado):
             siguiente = current.get_Next()
             self.eliminar_de_lista(solicitud.solicitudes, current)
             current = siguiente
-
-        Solicitud.toFile(solicitud, "Solicitudes_agregar.txt")
-        Solicitud.toFile(solicitud, "Solicitudes_eliminar.txt")
         
+        solicitudes_data = []
+        current = solicitudes_agregar.first()
+        while current is not None:
+            solicitudes_data.append(str(current.get_data()))
+            current= current.get_Next()
+        Solicitud.toFile(solicitudes_data, "Solicitudes_agregar.txt")
+#        guardar_datos("Solicitudes_agregar.txt", solicitudes_data)
+        
+        solicitudes_data_eliminar = []
+        current = solicitudes_eliminar.first()
+        while current is not None:
+            solicitudes_data_eliminar.append(str(current.get_data()))
+            current= current.get_Next()
+        Solicitud.toFile(solicitudes_data, "Solicitudes_eliminar.txt")
+#        guardar_datos("Solicitudes_eliminar.txt", solicitudes_data)
+
     #crear_usuario( usuario Usuario): Boolean
     #eliminar_usuario( id Int): Boolean
     #cambiar_contraseña( id Int, csñ String): Boolean
