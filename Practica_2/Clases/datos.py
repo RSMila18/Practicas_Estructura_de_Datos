4d5fac2364174a5eed6003f9633cd59895844f7b
Manue-droi <manusarf16@gmail.com>
2025-03-05 21:19:33 -0500

    .....

--- Practica_2/Clases/datos.py
+++ Practica_2/Clases/datos.py
@@ -9,9 +9,13 @@ class Datos:
         self.ruta_archivo = directorio_actual.parent / ruta_archivo
         
     def leer_archivo(self):
-        df = pd.read_csv(self.ruta_archivo, sep = ";", header =None, names=['A', 'B', 'KM', 'Minutos'])
+        df = pd.read_csv(self.ruta_archivo, sep=";", header=None, names=['A', 'B', 'KM', 'Minutos'])
+        df = df[1:].copy() 
         df["A"] = df["A"].str.strip()
         df["B"] = df["B"].str.strip()
+        df["KM"] = pd.to_numeric(df["KM"], errors="coerce")  
+        df["Minutos"] = pd.to_numeric(df["Minutos"], errors="coerce")
+        df = df.dropna(subset=["KM", "Minutos"])  
         return df
     
     def cargar_grafo(self, grafo):
--- Practica_2/Clases/grafo.py
+++ Practica_2/Clases/grafo.py
@@ -4,7 +4,7 @@ class Grafo:
     
     def __init__(self):
         self.grafo = nx.Graph()
-        
+
     def agregar_arista(self, ciudad_a, ciudad_b, distancia, tiempo):
         self.grafo.add_edge(ciudad_a, ciudad_b, distancia=distancia, tiempo=tiempo)
         
@@ -15,14 +15,14 @@ class Grafo:
         try:
             return nx.dijkstra_path(self.grafo, ciudad_a, ciudad_b, weight='distancia')
         except nx.NetworkXNoPath:
-            return f"No hay camino entre {ciudad_a} y {ciudad_b}"
+            return f"\nNo hay camino entre {ciudad_a} y {ciudad_b}\n"
         except nx.NodeNotFound:
-            return f"Una o ambas ciudades no se encuentra en el grafo"
+            return f"\nUna o ambas ciudades no se encuentra en el grafo\n"
 
     def camino_mas_corto_tiempo(self, ciudad_a, ciudad_b):
         try:
             return nx.dijkstra_path(self.grafo, ciudad_a, ciudad_b, weight='tiempo')
         except nx.NetworkXNoPath:
-            return f"No hay camino entre {ciudad_a} y {ciudad_b}"
+            return f"\nNo hay camino entre {ciudad_a} y {ciudad_b}\n"
         except nx.NodeNotFound:
-            return f"Una o ambas ciudades no se encuentra en el grafo"
+            return f"\nUna o ambas ciudades no se encuentra en el grafo\n"
