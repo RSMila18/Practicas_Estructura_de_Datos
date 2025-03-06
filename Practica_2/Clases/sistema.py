4de2c28268d88502cb505c4752b69c51239125c0
LuaN1409 <marestrepoo@unal.edu.co>
2025-03-05 19:29:21 -0500

    ....

--- Practica_2/Clases/sistema.py
+++ Practica_2/Clases/sistema.py
@@ -13,7 +13,33 @@ class Sistema:
         return ciudad_a, ciudad_b    
     
     def menu(self):
-        print("1. Verificar si dos ciudades están conectadas")
-        print("2. Encontrar el camino más corto por distancia entre dos ciudades")
-        print("3. Encontrar el camino más corto por tiempo entre dos ciudades")
-        print("4. Salir")
+        
+        while True:
+            print("1. Verificar si dos ciudades están conectadas")
+            print("2. Encontrar el camino más corto por distancia entre dos ciudades")
+            print("3. Encontrar el camino más corto por tiempo entre dos ciudades")
+            print("4. Salir")
+            
+            opcion = input("Seleccione una opción: ")
+            if opcion == "1":
+                ciudad_a, ciudad_b = self.solicitar_ciudades()
+                if self.grafo.estan_ciudades_conectadas(ciudad_a, ciudad_b):
+                    print(f"{ciudad_a} y {ciudad_b} están conectadas.")
+                else:
+                    print(f"{ciudad_a} y {ciudad_b} no están conectadas.")
+                    
+            elif opcion == "2":
+                ciudad_a, ciudad_b = self.solicitar_ciudades()
+                camino = self.grafo.camino_mas_corto_distancia(ciudad_a, ciudad_b)
+                print(f"El camino más corto entre {ciudad_a} y {ciudad_b} por la distancia es: {camino}")
+            
+            elif opcion == "3":
+                ciudad_a, ciudad_b = self.solicitar_ciudades()
+                camino = self.grafo.camino_mas_corto_tiempo(ciudad_a, ciudad_b)
+                print(f"El camino más corto entre {ciudad_a} y {ciudad_b} por el tiempo es: {camino}")
+                
+            elif opcion == "4":
+                break 
+            
+            else:
+                print("Opción inválida.")
