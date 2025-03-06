6a36647acf24aef6c8824b38bbfd6ee8ef2c450c
LuaN1409 <marestrepoo@unal.edu.co>
2025-03-05 20:04:12 -0500

    .....

--- Practica_2/Clases/datos.py
+++ Practica_2/Clases/datos.py
@@ -5,10 +5,12 @@ from Clases.grafo import Grafo
 class Datos:
     
     def __init__(self, ruta_archivo):
-        self.ruta_archivo = Path(__file__).parent / ruta_archivo
+        self.ruta_archivo = Path(__file__).parent
         
     def leer_archivo(self):
         df = pd.read_csv(self.ruta_archivo, sep = ";", header =None, names=['A', 'B', 'KM', 'Minutos'])
+        df["A"] = df["A"].str.strip()
+        df["B"] = df["B"].str.strip()
         return df
     
     def cargar_grafo(self, grafo):
